84158cc1e34ae4a672191e48c89f803db9d5dd27
Papoune <npapon@live.fr>
2020-06-05 19:28:19 +0200

    Servlet à jour

--- src/servlet/InscriptionServlet.java
+++ src/servlet/InscriptionServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,20 +13,31 @@ import javax.servlet.http.HttpSession;
 import bean.Utilisateur;
 import constante.Adressesinternes;
 import constante.AttributsServlet;
-import dao.DAOFactory;
 import dao.UtilisateurDao;
 import formulaire.InscriptionForm;
 
 @WebServlet( "/inscription" )
 public class InscriptionServlet extends HttpServlet {
+    // on injecte notre EJB
+    @EJB
+
+    /*
+     * Mais alors que nous devions initialiser manuellement le contenu de notre
+     * objet utilisateurDao // dans notre version précédente, cette fois nous
+     * nous reposons entièrement sur le conteneur : // à aucun moment nous ne
+     * créons ni n'initialisons une instance d'objet, // nous nous contentons
+     * uniquement de sa déclaration et de son annotation !
+     */
     private UtilisateurDao utilisateurDao;
 
-    public void init() throws ServletException {
-
-        utilisateurDao = ( (DAOFactory) this.getServletContext().getAttribute( AttributsServlet.DAOFACTORY ) )
-                .getUtilisateurDao();
-
-    }
+    /*
+     * On supprime tout ça :) public void init() throws ServletException {
+     * 
+     * utilisateurDao = ( (DAOFactory) this.getServletContext().getAttribute(
+     * AttributsServlet.DAOFACTORY ) ) .getUtilisateurDao();
+     * 
+     * }
+     */
 
     protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         this.getServletContext().getRequestDispatcher( Adressesinternes.INSCRIPTION ).forward( request,
